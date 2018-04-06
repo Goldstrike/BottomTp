c9ccc633d6ac903df29f90b1202a034f12381385
pc.dus <pc.dus@laposte.net>
2018-04-06 19:04:28 +0200

    Add of Naxam BottomTabbedPage

--- BottomTp/BottomTp.Android/MainActivity.cs
+++ BottomTp/BottomTp.Android/MainActivity.cs
@@ -6,6 +6,9 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Naxam.Controls.Platform.Droid;
+using Android.Graphics;
+using Plugin.Iconize;
 
 namespace BottomTp.Droid
 {
@@ -17,13 +20,65 @@ namespace BottomTp.Droid
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
 
+            // Naxam
+            
+            SetupBottomTabs();
+            Plugin.Iconize.Iconize
+                .With(new Plugin.Iconize.Fonts.FontAwesomeModule())
+                .With(new Plugin.Iconize.Fonts.MaterialModule());
+            
+
             base.OnCreate(bundle);
 
             // Iconize
-            Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
+            //Plugin.Iconize.Iconize.Init(Resource.Id.toolbar, Resource.Id.sliding_tabs);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
+        }
+
+        void SetupBottomTabs()
+        {
+            var stateList = new Android.Content.Res.ColorStateList(
+                new int[][] {
+                    new int[] { Android.Resource.Attribute.StateChecked
+                },
+                new int[] { Android.Resource.Attribute.StateEnabled
+                }
+                },
+                new int[] {
+                    Color.Red, //Selected
+                    Color.White //Normal
+	            });
+
+            BottomTabbedRenderer.BackgroundColor = new Color(0x9C, 0x27, 0xB0);
+            //BottomTabbedRenderer.FontSize = 12f;
+            BottomTabbedRenderer.FontSize = 14f;
+            BottomTabbedRenderer.IconSize = 16;
+            BottomTabbedRenderer.ItemTextColor = stateList;
+            BottomTabbedRenderer.ItemIconTintList = stateList;
+            //BottomTabbedRenderer.Typeface = Typeface.CreateFromAsset(this.Assets, "architep.ttf");
+            //BottomTabbedRenderer.Typeface = Typeface.CreateFromAsset(this.Assets, "HiraginoKakugoProNW3.otf");
+            BottomTabbedRenderer.ItemBackgroundResource = Resource.Drawable.bnv_selector;
+            BottomTabbedRenderer.ItemSpacing = 4;
+            //BottomTabbedRenderer.ItemPadding = new Xamarin.Forms.Thickness(6);
+            BottomTabbedRenderer.BottomBarHeight = 56;
+            BottomTabbedRenderer.ItemAlign = ItemAlignFlags.Center;
+            BottomTabbedRenderer.ShouldUpdateSelectedIcon = true;
+            BottomTabbedRenderer.MenuItemIconSetter = (menuItem, iconSource, selected) =>
+            {
+                var iconized = Iconize.FindIconForKey(iconSource.File);
+                if (iconized == null)
+                {
+                    BottomTabbedRenderer.DefaultMenuItemIconSetter.Invoke(menuItem, iconSource, selected);
+
+                    return;
+                }
+
+                var drawable = new IconDrawable(this, iconized).Color(selected ? Color.Red : Color.White).SizeDp(30);
+
+                menuItem.SetIcon(drawable);
+            };
         }
     }
 }
--- BottomTp/BottomTp/App.xaml.cs
+++ BottomTp/BottomTp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 
 using BottomTp.Views;
+using Naxam.Controls.Forms;
 using Xamarin.Forms;
 
 namespace BottomTp
@@ -17,11 +18,59 @@ namespace BottomTp
 
 
             // default
+            // -------
             //MainPage = new MainPage();
 
             // iconize
-            MainPage = new IcMainPage();
+            // --------
+            //MainPage = new IcMainPage();
+            //MainPage = new NavigationPage(new ItemsPage());
 
+            // Naxam
+            // -----
+            // Naxam - Github sample
+            /*
+            tabs.Children.Add(new ContentPage
+            {
+                Title = "Tab 1",
+                Icon = "md-view-list",
+                BackgroundColor = Color.Aqua,
+                Content = new Label
+                {
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    Text = "BottomTabbedPage - A Xamarin.Forms page with tabs at the bottom.",
+                    TextColor = Color.DarkCyan,
+                    Margin = new Thickness(16)
+                }
+            });
+            tabs.Children.Add(new ContentPage
+            {
+                Title = "Tab 2",
+                Icon = "md-help",
+                BackgroundColor = Color.Beige,
+                Content = new Label
+                {
+                    HorizontalTextAlignment = TextAlignment.Center,
+                    VerticalTextAlignment = TextAlignment.Center,
+                    Text = "BottomTabbedPage internally uses BottomNavigationViewEx.",
+                    TextColor = Color.Green,
+                    Margin = new Thickness(16)
+                }
+            });
+            MainPage = tabs;
+            */
+
+            // Naxam - default code behind
+            /*
+            var tabs = new BottomTabbedPage();
+            tabs.Children.Add(new ItemsPage() { Title = "Browse", Icon = "md-view-list" });
+            tabs.Children.Add(new AboutPage() { Title = "About", Icon = "md-help" });
+            MainPage = tabs;
+            */
+
+            // Naxam - default XAML
+            MainPage = new NaxamMainPage();
         }
 
 		protected override void OnStart ()
